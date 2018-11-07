2f6ddd36491e767e64257a5bc2d5fc2384c96bef
QuickCods <35273049+QuickCods@users.noreply.github.com>
2018-11-07 11:33:28 +0000

    contar caracteres

---
+++ teste1/aula5/ex5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+	int count = 0;
+	char ch;
+
+	while((ch = getchar()) != '\n'){
+		if(isalpha(ch))
+			count++;
+	}
+	printf("A frase contém %d caracteres\n", count);
+}
