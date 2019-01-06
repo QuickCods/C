251ca0c27b2480c84e1ebc017c1392171fdd12fc
QuickCods <35273049+QuickCods@users.noreply.github.com>
2019-01-06 19:03:38 +0000

    verdadeiro/falso sobre apontadores

---
+++ exame/aula10/ex2.c
@@ -0,0 +1,20 @@
+/*
+a) printf("%d",i)
+                  V
+b) scanf("%d",i)
+                  F
+c) scanf("%d",&i)
+                  V
+d) printf("%d",p)
+                  F
+e) printf("%d",*p)
+                  V
+f) scanf("%d",p)
+
+g) printf("%p",p)
+                  V
+h) printf("%p",i)
+                  F
+i) printf("%p",*p)
+                  F
+*/
