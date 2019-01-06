70e9fcbd6ed55e9caf8499dace62a854d5289c1e
QuickCods <35273049+QuickCods@users.noreply.github.com>
2019-01-06 19:11:46 +0000

    reescrever store_zeros com apontadores

---
+++ exame/aula10/ex6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//original
+/*
+void store_zeros(int vec[], int n)
+int i;
+for(i = 0; i<n; i++)
+vec[i] = 0;
+}
+*/
+
+void store_zeros(int vec[]){
+	while(*vec != '\0'){
+		*vec = 0;
+		*vec++;
+	}
+}
+
+int main(void){
+	int a[5] = {1};
+
+	store_zeros(a);
+
+	for(int i = 0; i < 5; i++)
+		printf("a[%d]=%d\n",i,a[i]);
+}
