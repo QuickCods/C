2b7a386a5a278b20d1432b89ade9524cf1c8eaad
QuickCods <35273049+QuickCods@users.noreply.github.com>
2019-01-21 18:22:51 +0000

    algoritmos de ordenacao

--- exame/ordenacao.c
+++ exame/ordenacao.c
@@ -68,3 +68,17 @@ void quicksort_rec(int vec[], int l, int u){
 	quicksort_rec(vec, l, m-1);   //senao caso recursivo
 	quicksort_rec(vec, m+1, u);   // ordenar vec[m+1..u]
 }
+
+//bubble sort
+
+void bubble_sort(int vec[], int n){
+	for(int i = 0; i < n - 1; i++){
+		for(int j = 0; j < n - i - 1; j++){
+			if(vec[j] > vec[j+1]){
+				int temp = vec[j];
+				vec[j] = vec[j+1];
+				vec[j+1] = temp;
+			}
+		}
+	}
+}
